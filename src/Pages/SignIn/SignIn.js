2cb8da5c00dc117308e5a9f84eed9eb80acd7956
Elnaz <elnazrezaee80@gmail.com>
2023-05-10 18:18:47 +0430

    chore: API added

--- src/Pages/SignIn/SignIn.js
+++ src/Pages/SignIn/SignIn.js
@@ -1,17 +1,128 @@
 import React from 'react'
 import Background from '../../Images/SignIn/SignInImg.png'
 import { Grid, TextField, InputAdornment, ThemeProvider } from '@mui/material'
-import { EmailRounded, VpnKey } from '@mui/icons-material'
+import { VpnKey } from '@mui/icons-material'
 import './SignIn.css'
 import { PTypography } from '../../Components/PersianComponent'
 import { PRoundButton } from '../../Components/PersianComponent'
 import Link from '@mui/material/Link'
 import { theme } from '../../Theme/ThemeGenerator'
+import { useState, useEffect } from 'react'
+import history from '../../history'
+import Navbar from '../../Components/Navbar/Navbar'
+import { Create } from '@mui/icons-material'
 
 function SignIn() {
+  const initialFormData = Object.freeze({
+    username: '',
+    password: '',
+  })
+  const [formData, updateFormData] = useState(initialFormData)
+  const [flagData, setFlagData] = useState(false)
+  const [errorData, updateErrorData] = useState(initialFormData)
+  const [refresh, setRefresh] = useState(false)
+
+  useEffect(() => {
+    console.log('access_token Come')
+    console.log(localStorage.getItem('access_token'))
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        Authorization: 'JWT ' + localStorage.getItem('access_token'),
+        'Content-Type': 'application/json',
+      },
+    }
+    fetch('http://127.0.0.1:8000/user/login/', requestOptions)
+      .then((response) => {
+        console.log(localStorage.getItem('access_token'))
+        if (response.status != 401) {
+          history.push('/HomePage')  
+        }
+      })
+      .catch((err) => {})
+  }, [flagData])
+
+  useEffect(() => {
+    updateErrorData(initialFormData)
+    console.log(errorData)
+  }, [refresh])
+
+  const handleChange = (e) => {
+    updateFormData({
+      ...formData,
+      [e.target.name]: e.target.value.trim(),
+    })
+    updateErrorData({
+      ...errorData,
+      [e.target.name]: '',
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(formData)
+
+    if (refresh) setRefresh(false)
+    else setRefresh(true)
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username: formData.username,
+        password: formData.password,
+      }),
+    }
+    fetch('http://127.0.0.1:8000/user/refresh-token/', requestOptions)
+      .then((response) => {
+        if (response.status == 200) {
+          console.log('response')
+          response.json().then((data) => {
+            console.log(data)
+            localStorage.setItem('access_token', data.access)
+            localStorage.setItem('refresh_token', data.refresh)
+            setFlagData(flagData ? false : true)
+          })
+
+          //console.log(data)
+        } else {
+          throw response
+        }
+      })
+      .catch((err) => {
+        if (err.status === 401) {
+          alert('Your email or password is incorrect!')
+        }
+        err.text().then((errorMessage) => {
+          const errors = JSON.parse(errorMessage)
+          console.log('e ' + errors.username)
+          if (errors.username !== undefined) {
+            updateErrorData({
+              ...errorData,
+              username: errors.username,
+            })
+            return
+          }
+
+          if (errors.password !== undefined) {
+            updateErrorData({
+              ...errorData,
+              password: errors.password,
+            })
+            return
+          }
+        })
+      })
+  }
   return (
     <div>
       <ThemeProvider theme={theme}>
+      <Navbar
+        SearchOption={true}
+        TicketOption={false}
+        CartOption={false}
+        DrawerOption={false}
+        AuthorizationOption={false}
+      />
         <Grid
           container
           style={{ minHeight: '100vh' }}
@@ -36,7 +147,6 @@ function SignIn() {
           >
             <div />
             <div
-            dir='rtl'
               style={{
                 display: 'flex',
                 flexDirection: 'column',
@@ -44,36 +154,40 @@ function SignIn() {
                 minWidth: 300,
               }}
             >
+            <TextField
+              id='name'
+              name='name'
+              variant='standard'
+              label='Username'
+              margin='normal'
+              helperText={errorData.username != '' ? errorData.username : ''}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position='start'>
+                    {' '}
+                    <Create/>{' '}
+                  </InputAdornment>
+                ),
+              }}
+              onChange={handleChange}
+            />
               <TextField
-                className='textFieldContainer'
-                variant='standard'
-                name='email'
-                label='ایمیل'
-                margin='normal'
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position='start'>
-                      {' '}
-                      <EmailRounded />{' '}
-                    </InputAdornment>
-                  ),
-                }}
-              />
-              <TextField
-                variant='standard'
-                type='password'
-                name='password'
-                label='رمز عبور'
-                margin='normal'
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position='start'>
-                      {' '}
-                      <VpnKey />{' '}
-                    </InputAdornment>
-                  ),
-                }}
-              />
+              variant='standard'
+              type='password'
+              name='password'
+              label='Password'
+              margin='normal'
+              helperText={errorData.password != '' ? errorData.password : ''}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position='start'>
+                    {' '}
+                    <VpnKey />{' '}
+                  </InputAdornment>
+                ),
+              }}
+              onChange={handleChange}
+            />
               <div
                 style={{
                   display: 'flex',
@@ -84,9 +198,9 @@ function SignIn() {
                 }}
               />
               <PRoundButton variant='contained' alignContent = 'center'>
-                <Link href='#' underline='none' color='white'>
+                <Link href='#' underline='none' color='white' onClick={handleSubmit}>
                   <PTypography variant='h6' color={theme.palette.text}>
-                    ورود
+                    LogIn
                   </PTypography>
                 </Link>
               </PRoundButton>
@@ -94,7 +208,7 @@ function SignIn() {
               <div className='divSignUp'>
                 <Link href='/signup' underline='none' color='primary'>
                   <PTypography variant='h6' color={theme.palette.text}>
-                    ثبت نام
+                    SIGN UP
                   </PTypography>
                 </Link>
               </div>
