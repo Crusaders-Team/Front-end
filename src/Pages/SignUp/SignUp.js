cc551821794396bd6d8c7452e8fc7de710db35e3
Elnaz <elnazrezaee80@gmail.com>
2023-05-10 18:19:22 +0430

    chore: API added

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Background from '../../Images/SignUp/SignUpImg.png'
-import { Grid, TextField, Button, InputAdornment } from '@mui/material'
+import { Grid, TextField, InputAdornment } from '@mui/material'
 import { AccountCircle, VpnKey, EmailSharp, Create } from '@mui/icons-material'
 import history from '../../history'
 import './SignUp.css'
@@ -8,12 +8,120 @@ import { theme } from '../../Theme/ThemeGenerator'
 import { PTypography } from '../../Components/PersianComponent'
 import { PRoundButton } from '../../Components/PersianComponent'
 import Link from '@mui/material/Link'
-import PhoneEnabledIcon from '@mui/icons-material/PhoneEnabled';
+import Typography from '@mui/material/Typography';
+import Navbar from '../../Components/Navbar/Navbar';
+import { useForm } from "react-hook-form";
+import axios from "axios";
 
 function SignUp() {
 
+  const initialFormData = Object.freeze({
+    userName: '',
+    email: '',
+    password: '',
+  })
+  const [formData, updateFormData] = useState(initialFormData)
+  const [errorData, updateErrorData] = useState(initialFormData)
+  const [refresh, setRefresh] = useState(false)
+  
+
+  const handleChange = (e) => {
+    updateFormData({
+      ...formData,
+      [e.target.name]: e.target.value.trim(),
+    })
+    updateErrorData({
+      ...errorData,
+      [e.target.name]: '',
+    })
+    console.log(formData)
+  }
+  const {
+    register,
+    formState: { errors },
+  } = useForm();
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(formData)
+    console.log(
+      JSON.stringify({
+        username: formData.userName,
+        email: formData.email,
+        password: formData.password,
+      })
+    )
+    updateErrorData({
+      ...errorData,
+      userName: '',
+    })
+    updateErrorData({
+      ...errorData,
+      email: '',
+    })
+    updateErrorData({
+      ...errorData,
+      password: '',
+    })
+    console.log(errorData)
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username: formData.userName,
+        email: formData.email,
+        password: formData.password,
+      }),
+    }
+    fetch('http://127.0.0.1:8000/user/signup/', requestOptions)
+      .then((response) => {
+        if (response.status === 201) {
+          alert('User registered!')
+          history.push('/signin')
+          window.location.reload(true)
+        } else {
+          throw response
+        }
+      })
+      .catch((err) => {
+        err.text().then((errorMessage) => {
+          const errors = JSON.parse(errorMessage)
+          console.log('e ' + errors.email)
+          if (errors.username !== undefined) {
+            updateErrorData({
+              ...errorData,
+              userName: errors.username,
+            })
+            return
+          }
+
+          if (errors.email !== undefined) {
+            updateErrorData({
+              ...errorData,
+              email: errors.email,
+            })
+            return
+          }
+
+          if (errors.password !== undefined) {
+            updateErrorData({
+              ...errorData,
+              password: errors.password,
+            })
+            return
+          }
+        })
+      })
+  }
   return (
     <div>
+      <Navbar
+        SearchOption={true}
+        TicketOption={false}
+        CartOption={false}
+        DrawerOption={false}
+        AuthorizationOption={false}
+      />
       <Grid
         container
         style={{ minHeight: '100vh' }}
@@ -38,7 +146,6 @@ function SignUp() {
         >
           <div />
           <div 
-            dir='rtl'
             style={{
               display: 'flex',
               flexDirection: 'column',
@@ -49,26 +156,13 @@ function SignUp() {
             }}
           >
             <TextField
-              id='name'
-              name='name'
+              id='userName'
+              name='userName'
               variant='standard'
-              label='نام'
+              label='Username'
               margin='normal'
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position='start'>
-                    {' '}
-                    <Create />{' '}
-                  </InputAdornment>
-                ),
-              }}
-            />
-            <TextField
-              id='lastName'
-              name='lastName'
-              variant='standard'
-              label='نام کاربری'
-              margin='normal'
+              required
+              helperText={errorData.userName != '' ? errorData.userName : ''}
               InputProps={{
                 startAdornment: (
                   <InputAdornment position='start'>
@@ -77,28 +171,24 @@ function SignUp() {
                   </InputAdornment>
                 ),
               }}
-            />
-            <TextField
-              id='userName'
-              name='userName'
-              variant='standard'
-              label='شماره تماس'
-              margin='normal'
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position='start'>
-                    {' '}
-                    <PhoneEnabledIcon />{' '}
-                  </InputAdornment>
-                ),
-              }}
+              onChange={handleChange}
             />
             <TextField
               id='email'
               name='email'
               variant='standard'
-              label='ایمیل'
+              label='Email'
               margin='normal'
+              required
+              {...register("email", {
+                required: "Required field",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "Invalid email address",
+                },
+              })}
+              error={!!errors?.email}
+              helperText={errors?.email ? errors.email.message : null}
               InputProps={{
                 startAdornment: (
                   <InputAdornment position='start'>
@@ -107,14 +197,17 @@ function SignUp() {
                   </InputAdornment>
                 ),
               }}
+              onChange={handleChange}
             />
             <TextField
               id='password'
               name='password'
               variant='standard'
               type='password'
-              label='رمز عبور'
+              label='Password'
               margin='normal'
+              required
+              helperText={errorData.password != '' ? errorData.password : ''}
               InputProps={{
                 startAdornment: (
                   <InputAdornment position='start'>
@@ -123,21 +216,22 @@ function SignUp() {
                   </InputAdornment>
                 ),
               }}
+              onChange={handleChange}
             />
             <div style={{ height: 20 }} />
             <PRoundButton variant='contained' alignContent = 'center'>
-              <Link href='#' underline='none' color='white'>
+              <Link href='#' underline='none' color='white' onClick={handleSubmit}>
                 <PTypography variant='h6' color={theme.palette.text}>
-                  ثبت نام
+                  Sign Up
                 </PTypography>
               </Link>
             </PRoundButton>
             <div style={{ height: 30 }} className='Buttons' />
             <div className='divSignUp'>
-              <Link href='/signin' underline='none' color='primary'>
-                <PTypography variant='h6' color={theme.palette.text}>
-                  ورود
-                </PTypography>
+              <Link href='/signin' underline='none' color='primary' onClick={() => history.push('/signin')}>
+                <Typography variant='h6' color={theme.palette.text}>
+                Have an account?
+                </Typography>
               </Link>
             </div>
           </div>
